020329ebcc22f35da03f2fd311d75737e278f8ca
Michael-Bissinger <Michael.Bissinger@inb.de>
2019-08-19 17:27:37 +0200

    create scripts and prefabs to set arrow-movement

---
+++ Arrow-Game/Assets/Scenes/Shoot arrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootarrow : MonoBehaviour
+{
+
+    GameObject arrow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                
+                // spawn an arrow object on the arrow-shooter position
+
+
+            }
+        }
+    }
+}
---
+++ Arrow-Game/Assets/Scripts/ArrowMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour
+{
+
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the object forward along its z axis 1 unit/second.
+        transform.position += Vector3.left;
+
+        // Move the object upward in world space 1 unit/second.
+        //transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+    }
+}
