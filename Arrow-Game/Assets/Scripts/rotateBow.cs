2becc55097a059362390bb620b26f2ed9d0544e9
Michael-Bissinger <Michael.Bissinger@inb.de>
2019-08-20 11:15:45 +0200

    change direction after -40f rotation is reached

--- Arrow-Game/Assets/Scripts/rotateBow.cs
+++ Arrow-Game/Assets/Scripts/rotateBow.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class rotateBow : MonoBehaviour
 {
+    private float direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (direction == 1f)
+        {
+
+            if (transform.Rotate(new Vector3(0f, 0f, -40f)))
+            {
+                // If this point is reached, then change the direction
+
+                direction = -1f;
+            }
+
+
+        }
+
+
+        transform.Rotate(new Vector3(0f, 0f, -direction));
+
+
     }
 }
